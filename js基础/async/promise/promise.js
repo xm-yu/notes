6ec2ac982783b1891ec037f45c988ba8aa26653c
余志成 <yuzhicheng@yuzhichengdeMacBook-Pro-2.local>
2021-07-09 02:54:10 +0800

    feat(async-pool): ✨  异步并发控制

--- js基础/async/promise/promise.js
+++ js基础/async/promise/promise.js
@@ -8,7 +8,6 @@ const myFetch = async url => {
     const response = await fetch(url);
     const json = await response.json();
     throw new Error('error');
-
     return json;
   } catch (error) {
     return Promise.reject(error);
@@ -94,7 +93,7 @@ const myFetch = async url => {
   console.log('2');
 }
 
-// promise A+规明确规定 onFulfilled和 onRejected方法异步执行， 切应该在then方法被调用的那轮事件循环之后的新执行栈中执行
+// promise A+规明确规定 onFulfilled和 onRejected方法异步执行， 应该在then方法被调用的那轮事件循环之后的新执行栈中执行
 
 // 正确使用Promise的方式
 // async 使用 promise all
@@ -132,4 +131,148 @@ const myFetch = async url => {
     .catch(error => {
       console.log(error);
     });
+}
+
+/**
+ * @断开的的promise链
+ */
+
+{
+  const myFetch = async url => {
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      return json;
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  };
+
+  // bad 这个例子断开promise 链 如果somethingComplicated 发生错误，返回出得promise 无法通过catch 捕获到错误
+  const anAsyncCall1 = url => {
+    const promise = myFetch(url);
+    promise.then(data => {
+      throw new Error('an async call throw error');
+    });
+    return promise;
+  };
+
+  anAsyncCall1(mockFetchUrl).catch(error => {
+    // 这里不能捕获 anAsyncCall 里面 promise的错误
+    console.log('anAsyncCall1 catch error', error);
+  });
+
+  const anAsyncCall2 = url => {
+    const promise = myFetch(url);
+    return promise.then(data => {
+      throw new Error('an async call throw error');
+    });
+  };
+
+  anAsyncCall2(mockFetchUrl).catch(error => {
+    // 这里promise 链·上了 能捕获到错误
+    console.log('anAsyncCall2 catch error', error);
+  });
+}
+
+// promise 并发控制
+{
+  const urlTpl = 'https://jsonplaceholder.typicode.com/posts';
+  const urls = [];
+  for (let index = 0; index < 2; index++) {
+    // if (index % 2 === 0) {
+    //   urls.push('fdafasf');
+    // }
+    urls.push(`${urlTpl}/${index + 1}`);
+  }
+
+  const requestQueue = urls => {
+    const len = urls.length;
+    const results = [];
+    let counter = 0;
+    return new Promise(resolve => {
+      const request = index => {
+        counter += 1;
+        const url = urls[index];
+        fetch(url)
+          .then(response => {
+            return response.json();
+          })
+          .then(json => {
+            results.push(json);
+            if (counter === len) {
+              resolve(results);
+            } else {
+              request(++index);
+            }
+          });
+      };
+
+      request(0);
+    });
+  };
+
+  // requestQueue(urls).then(result => {
+  //   console.log(result);
+  // });
+
+  const requestQueueReduce = urls => {
+    console.log(urls);
+    return urls.reduce(
+      (promise, curUrl) =>
+        promise.then(results =>
+          fetch(curUrl)
+            .then(response => response.json())
+            .then(json => [...results, json])
+            .catch(err => {
+              return [...results, 'error'];
+            })
+        ),
+      Promise.resolve([])
+    );
+  };
+
+  // requestQueueReduce(urls).then(result => {
+  //   console.log(result);
+  // });
+
+  const limitRequestConcurrency = (urls, max = 1) => {
+    return new Promise((resolve, reject) => {
+      if (!Array.isArray(urls)) {
+        // todo 简单判断下基本的条件 还可以添加其他细节的限制
+        reject('参数不合法啊');
+      }
+      const len = urls.length;
+      const results = new Array(len).fill(false);
+      let sendCount = 0;
+      let finishCount = 0;
+
+      const request = () => {
+        const index = sendCount++;
+        console.log('run index ', index, urls[index]);
+        if (index >= len) return;
+        const url = urls[index];
+        fetch(url)
+          .then(response => response.json())
+          .then(json => {
+            results[index] = json;
+          })
+          .catch(err => {
+            results[index] = err;
+          })
+          .finally(() => {
+            finishCount += 1;
+            if (finishCount === len) return resolve(results);
+            request();
+          });
+      };
+      // 先达到最大并发数量
+      while (sendCount < max && sendCount < len) {
+        request();
+      }
+    });
+  };
+  limitRequestConcurrency(urls, 3).then(data => {
+    console.log(data);
+  });
 }
